f4e1b4bf5be80c7c6c9adcfdbb16ffb1e0477283
lhj41 <lhj41@DESKTOP-RMSD2LA>
2018-11-25 00:59:53 +0900

    디테일페이지에서 사이즈별로 파라미터 전송

--- ex01/src/main/java/org/oos/controller/OrderController.java
+++ ex01/src/main/java/org/oos/controller/OrderController.java
@@ -1,8 +1,16 @@
 package org.oos.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.oos.domain.OrderDetailVO;
+import org.oos.domain.ProductOptionVO;
+import org.oos.domain.ProductVO;
 import org.oos.service.MemberService;
 import org.oos.service.OrderDetailService;
 import org.oos.service.OrderService;
+import org.oos.service.ProductService;
 import org.oos.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -30,6 +38,9 @@ public class OrderController {
 	@Setter(onMethod_=@Autowired)
 	private StoreService storeService;
 	
+	@Setter(onMethod_=@Autowired)
+	private ProductService productService;
+	
 	@GetMapping("/success")
 	public void successGET(Long ono, Model model) {
 		
@@ -37,11 +48,32 @@ public class OrderController {
 	}
 	
 	@GetMapping("/list")
-	public void orderListGET(String mid, Long sno, Model model) {
+	public void orderListGET(Long pno,String mid, Long sno,String[] info, Model model) {
 		
+		log.info("------------------------------------------");
+		log.info("info: "+Arrays.toString(info));
+		
+		List<OrderDetailVO> list=new ArrayList<>();
+		for (String size : info) {
+			String[] sizeInfo=size.split("_");
+			OrderDetailVO vo=new OrderDetailVO();			
+			
+			ProductVO pVO=productService.read(pno);
+			pVO.getOptList().forEach(opt->{
+				if(opt.getSize().equals(sizeInfo[0])) {
+					vo.setOpno(opt.getOpno());	
+				}
+			});
+			vo.setPno(pno);
+			vo.setProduct(pVO);
+			vo.setQty(Long.parseLong(sizeInfo[1]));
+			
+			list.add(vo);
+			log.info(""+vo);
+		}
 		model.addAttribute("Member", memberService.get(mid));
 		model.addAttribute("Store", storeService.get(sno));
-		
+		model.addAttribute("orderList",list);
 	}
 
 }
